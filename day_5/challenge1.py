e7bc5a5b4f91a810edb11e8baf471ceccab4738d
Jamie Brynes <jamiebrynes7@gmail.com>
2017-12-08 19:58:28 +0000

    completed day 5 challenges

---
+++ day_5/challenge1.py
@@ -0,0 +1,26 @@
+def main():
+
+    # Parse problem input
+    input_fp = "./input.txt"
+    instructions = []
+    with open(input_fp, 'r') as input_file:
+        for line in input_file:
+            line = line.replace('\n', '')
+            instructions.append(int(line))
+
+    current_pos = 0
+    steps = 0
+    while True:
+        steps += 1
+        next_pos = current_pos + instructions[current_pos]
+        instructions[current_pos] += 1
+        current_pos = next_pos
+        if current_pos >= len(instructions):
+            break
+    
+    print("The answer is: " + str(steps))
+
+
+
+if __name__ == "__main__":
+    main()

041b91d2c2b0ee8a8accf98450b50e14fce4e3b1
Jamie Brynes <jamiebrynes7@gmail.com>
2017-12-27 14:46:17 +0000

    completed day 11 challenges

---
+++ day_11/challenge2.py
@@ -0,0 +1,39 @@
+def main():
+
+    steps = []
+    with open('./input.txt', 'r') as f:
+        steps = f.read().replace("\n", "").split(",")
+    
+    location = [0,0,0]
+    max_distance = 0
+    for step in steps:
+        if step == "n":
+            incrementLocation(location, [0, 1, -1])
+        elif step == "ne":
+            incrementLocation(location, [-1, 1, 0])
+        elif step == "se":
+            incrementLocation(location, [-1, 0, 1])
+        elif step == "s":
+            incrementLocation(location, [0, -1, 1])
+        elif step == "sw":
+            incrementLocation(location, [1, -1, 0])
+        elif step == "nw":
+            incrementLocation(location, [1, 0, -1])
+        else:
+            print("Unrecognized instruction: ")
+            print(step)
+        max_distance = getDistance(location) if getDistance(location) > max_distance else max_distance
+    
+    print(max_distance)
+
+def incrementLocation(location, change):
+    for i in range(len(change)):
+        location[i] += change[i]
+
+def getDistance(location):
+    return max([abs(coord) for coord in location])
+
+
+
+if __name__ == "__main__":
+    main()
